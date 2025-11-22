784da89bfab52dbbeb2883290533eba9b6ce5e8e
AdyanthG <AdyanthGanesh@gmail.com>
2025-11-22 14:22:51 -0500

    favicon

---
+++ frontend/app/icon.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    fontSize: 20,
+                    background: 'black',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'white',
+                    borderRadius: '6px',
+                    transform: 'rotate(-3deg)',
+                }}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+                    <path d="m15 5 4 4" />
+                </svg>
+            </div>
+        ),
+        {
+            ...size,
+        }
+    )
+}
